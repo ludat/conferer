0b3fd91ae4466a2cab6a22051d7ddd51bf5d77f8
Lucas David Traverso <lucas6246@gmail.com>
2019-06-16 17:15:01 +0200

    Added the example and some other fixups

---
+++ example/app/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Conferer
+import Conferer.FetchFromConfig.Warp
+
+import Network.Wai
+import Network.HTTP.Types (status200)
+import Network.Wai.Handler.Warp (runSettings, getPort)
+
+main :: IO ()
+main = do
+  config <- defaultConfig "awesomeapp"
+  warpSettings <- getFromConfig "warp" config
+
+  putStrLn $ "Running on port: " ++ show (getPort warpSettings)
+  runSettings warpSettings application
+
+application _ respond = respond $
+  responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
+
