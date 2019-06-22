ed55840f8780a9d2b990edfbb9f6670df684f99f
Lucas David Traverso <lucas6246@gmail.com>
2019-06-22 19:15:29 +0200

    Remove usages of deriving via to support older ghc

--- conferer/src/Conferer/FetchFromConfig/Basics.hs
+++ conferer/src/Conferer/FetchFromConfig/Basics.hs
@@ -1,37 +1,43 @@
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, DerivingVia, StandaloneDeriving #-}
-
+{-# LANGUAGE FlexibleInstances #-}
 module Conferer.FetchFromConfig.Basics where
-import Conferer.Types
-import Conferer.Core (getKey)
-import Data.Text (Text, unpack, toLower)
-import Data.ByteString (ByteString)
-import Data.String (IsString, fromString)
-import Text.Read (readMaybe)
 
-deriving via FetchAsRead Int instance FetchFromConfig Int
-deriving via FetchAsRead Float instance FetchFromConfig Float
+import           Conferer.Types
+import           Conferer.Core (getKey)
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import           Data.ByteString (ByteString)
 
-newtype FetchAsRead a = FetchAsRead { getFetchAsRead :: a }
+import           Data.String (IsString, fromString)
 
-instance (Read a) => FetchFromConfig (FetchAsRead a) where
-    fetch = fetchFromConfigWith parseReadable
-        where parseReadable text = FetchAsRead <$> (readMaybe $ unpack $ text)
+import           Text.Read (readMaybe)
+
+instance FetchFromConfig Int where
+  fetch = fetchFromConfigByRead
+
+instance FetchFromConfig Float where
+  fetch = fetchFromConfigByRead
+
+fetchFromConfigByRead :: Read a => Key -> Config -> IO (Either Text a)
+fetchFromConfigByRead = fetchFromConfigWith (readMaybe . Text.unpack)
+
+instance FetchFromConfig ByteString where
+  fetch = fetchFromConfigWith (Just . Text.encodeUtf8)
+
+instance FetchFromConfig String where
+  fetch = fetchFromConfigWith (Just . Text.unpack)
+
+instance FetchFromConfig Text where
+  fetch = fetchFromConfigWith (Just)
 
 instance FetchFromConfig Bool where
     fetch = fetchFromConfigWith parseBool
-        where parseBool text = case toLower text of
-                                    "false" -> Just False
-                                    "true" -> Just True
-                                    _ -> Nothing
-
-newtype FromString a = FromString { getFromString :: a }
-
-instance (IsString a) => FetchFromConfig (FromString a) where
-    fetch = fetchFromConfigWith (pure . FromString . fromString . unpack)
-
-deriving via FromString [Char] instance FetchFromConfig [Char]
-deriving via FromString ByteString instance FetchFromConfig ByteString
-deriving via FromString Text instance FetchFromConfig Text
+        where
+          parseBool text =
+            case Text.toLower text of
+              "false" -> Just False
+              "true" -> Just True
+              _ -> Nothing
 
 fromValueWith :: (Text -> Maybe a) -> Key -> Text -> Either Text a
 fromValueWith parseValue key valueAsText = case parseValue valueAsText of
