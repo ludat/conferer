f4c57f7bd335a7bbce4ce0a58181a1abb1afbaba
Lucas David Traverso <lucas6246@gmail.com>
2021-01-19 19:05:50 -0300

    Added example for blogpost and removed default extensions to allow runhaskell

---
+++ example/src/AnnouncementBlogpost.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+module AnnouncementBlogpost where
+
+import qualified Conferer
+import Conferer.FromConfig.Warp ()
+import GHC.Generics
+
+import Network.Wai
+import Network.HTTP.Types (status200)
+import qualified Network.Wai.Handler.Warp as Warp
+
+import qualified Conferer.Source.CLIArgs as Cli
+import qualified Conferer.Source.Env as Env
+import qualified Conferer.Source.Dhall as Dhall
+
+data AppConfig = AppConfig
+  { appConfigServer :: Warp.Settings
+  , appConfigBanner :: String
+  } deriving (Generic)
+instance Conferer.FromConfig AppConfig
+instance Conferer.DefaultConfig AppConfig where
+ configDef = AppConfig
+   { appConfigServer = Conferer.configDef
+   , appConfigBanner = "Hi conferer"
+   }
+
+mkMyConfig :: IO Conferer.Config
+mkMyConfig = Conferer.mkConfig' []
+  [ Cli.fromConfig
+  , Env.fromConfig "myapp"
+  , Dhall.fromFilePath "config.dhall"
+  ]
+
+
+main :: IO ()
+main = do
+  config <- mkMyConfig
+  appConfig <- Conferer.fetch config
+  let warpSettings = appConfigServer appConfig
+
+  putStrLn $ appConfigBanner appConfig
+  putStrLn $ "Running on port: " ++ show (Warp.getPort $ warpSettings)
+  Warp.runSettings warpSettings application
+
+application :: Application
+application _ respond = do
+ respond $
+   responseLBS status200 [("Content-Type", "text/plain")] "Hi"
